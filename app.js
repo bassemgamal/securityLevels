e0b82e44a2f8bbe3934f0db639fa63b5c966581b
bassemgamal <mozooo19@gmail.com>
2024-11-24 06:03:17 +0200

    Level 4 Security - Hashing & Salt Encryption.

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcypt = require("bcrypt");
+const saltRound = 10;
 const app = express();
 const port = 3000;
 
@@ -34,32 +35,36 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
-  newUser
-    .save()
-    .then(() => {
-      res.render("secrets");
-    })
-    .catch((err) => {
-      console.log(err);
+  bcypt.hash(req.body.password, saltRound, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
     });
+    newUser
+      .save()
+      .then(() => {
+        res.render("secrets");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 });
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
-
+  const password = req.body.password;
   User.findOne({ email: username })
     .then((user) => {
       if (user) {
-        if (user.password === password) {
-          res.render("secrets");
-        } else {
-          console.log("Wrong Password");
-        }
+        bcypt.compare(password, user.password, function (err, result) {
+          if (result === true) {
+              res.render("secrets");
+          }else{
+            console.log("Wrong password");
+            
+          }
+        });
       } else {
         console.log("Email not registerd");
       }
